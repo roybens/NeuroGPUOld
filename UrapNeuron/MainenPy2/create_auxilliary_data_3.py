045721e227164746f2b5bae175e19295b3d87c85
roybens <bens.roy@gmail.com>
2017-08-20 11:37:20 -0700

    Fixied indexing in create_aux_data

--- UrapNeuron/MainenPy2/create_auxilliary_data_3.py
+++ UrapNeuron/MainenPy2/create_auxilliary_data_3.py
@@ -1,10 +1,13 @@
 from make_tree_from_parent_vec import make_tree_from_parent_vec
+from collections import OrderedDict
 from auxilliary import Aux
 import numpy as np
 import cell
 from file_io import *
 from get_parent_from_neuron import get_parent_from_neuron
 import scipy.io as sio
+from cStringIO import StringIO
+import csv
 
 # input_dict = clean_creat_aux_3_mat(load_creat_aux_3_mat('/home/devloop0/inputCreatAux3.mat'))
 # A = input_dict['A']
@@ -99,9 +102,9 @@ def create_auxilliary_data_3(A, N, NSeg, Parent, cmVec,parent_seg,bool_model,seg
 	RelStarts = tree_dict['RelStarts']
 	RelEnds = tree_dict['RelEnds']
 	
-	aux.RelVec = RelVec
-	aux.RelStarts = RelStarts
-	aux.RelEnds = RelEnds
+	aux.RelVec = np.add(RelVec,1)
+	aux.RelStarts = np.add(RelStarts,1)
+	aux.RelEnds = np.add(RelEnds,1)
 	
 	LastLevelsI = np.where(Level == np.max(Level))[0][0] + 1
 	EndLastLevelsI = SegEndI[LastLevelsI - 1]
@@ -112,21 +115,52 @@ def create_auxilliary_data_3(A, N, NSeg, Parent, cmVec,parent_seg,bool_model,seg
 	FN = '../../Data/BasicConst' + str(N) + 'Seg.mat'
 	FNP = '../../Data/BasicConst' + str(N) + 'SegP.mat'
 	FNM = '../../Data/ParamsMat' + str(N) + '.mat'
+
+	FN_csv = '../../Data2/BasicConst' + str(N) + 'Seg.csv'
+	FNP_csv = '../../Data2/BasicConst' + str(N) + 'SegP.csv'
+
+        FN_uint16 = '../../Data2/BasicConst' + str(N) + 'Seg_uint16.mat'
+        FN_double = '../../Data2/BasicConst' + str(N) + 'Seg_double.mat'
+
+        FNP_uint16 = '../../Data2/BasicConst' + str(N) + 'SegP_uint16.mat'
+        FNP_double = '../../Data2/BasicConst' + str(N) + 'SegP_double.mat'
 	
 	aux.d = d
 	aux.e = e
 	aux.f = f
 	aux.Cms = cmVec
 	
-	FN_dict = {}
+	FN_dict = OrderedDict()
 	FN_dict['N'] = np.array([np.uint16(N)])
 	FN_dict['e'] = np.double(e)
 	FN_dict['f'] = np.double(f)
 	FN_dict['Ks'] = np.uint16(Ks)
 	FN_dict['auxCms'] = np.double(aux.Cms);
 	FN_dict['nrnHasHH'] = np.uint16(bool_model)
-	
+        FN_data = ''
+        for k in FN_dict:
+            s = StringIO()
+            np.savetxt(s, FN_dict[k].flatten(), fmt='%.5f', newline=',')
+            st = s.getvalue()
+            FN_data += st + '\n'
+        with open(FN_csv, 'w') as fn_f:
+            fn_f.write(FN_data)
+
 	sio.savemat(FN, FN_dict)
+
+        FN_dict_uint16 = {}
+        FN_dict_uint16['N'] = np.uint16(N)
+        FN_dict_uint16['Ks'] = np.uint16(Ks)
+        FN_dict_uint16['nrnHasHH'] = np.uint16(bool_model)
+        sio.savemat(FN_uint16, FN_dict_uint16)
+
+        FN_dict_double = {}
+        FN_dict_double['e'] = np.double(e)
+        FN_dict_double['f'] = np.double(f)
+        FN_dict_double['auxCms'] = np.double(aux.Cms)
+        sio.savemat(FN_double, FN_dict_double)
+
+
 	
 	CompByLevel32 = np.zeros((0, 32))
 	CompByFLevel32 = np.zeros((0, 32))
@@ -190,14 +224,14 @@ def create_auxilliary_data_3(A, N, NSeg, Parent, cmVec,parent_seg,bool_model,seg
 	CallForFathers = tree_dict['CallForFathers']
 	nCallForFather = CallForFathers.size
 	aux.nCallForFather = nCallForFather
-	FNP_dict = {}
+	FNP_dict = OrderedDict()
 	FNP_dict['N'] = np.array(np.uint16([N]))
 	FNP_dict['NComps'] = np.array(np.uint16([NComps]))
 	FNP_dict['e'] = np.double(e)
 	FNP_dict['f'] = np.double(f)
-	FNP_dict['Ks'] = np.uint16(Ks)
-	FNP_dict['SegToComp'] = np.uint16(np.subtract(seg_to_comp, 1))
-	FNP_dict['cmVec'] = np.double(cmVec)
+        FNP_dict['Ks'] = np.uint16(Ks)
+        FNP_dict['SegToComp'] = np.uint16(seg_to_comp)
+        FNP_dict['cmVec'] = np.double(cmVec)
 	FNP_dict['nrnHasHHSize'] = np.array(np.uint16([bool_model.shape[0]]))
 	FNP_dict['nrnHasHHT'] = bool_model.T
 	FNP_dict['SonNoVec'] = np.uint16(SonNoVec)
@@ -205,29 +239,75 @@ def create_auxilliary_data_3(A, N, NSeg, Parent, cmVec,parent_seg,bool_model,seg
 	FNP_dict['LognDepth'] = np.array(np.uint16([LognDepth]))
 	FNP_dict['nFathers'] = np.array(np.uint16([nFathers]))
 	FNP_dict['nCallForFather'] = np.array(np.uint16([nCallForFather]))
-	FNP_dict['RelStarts'] = np.uint16(RelStarts)
-	FNP_dict['RelEnds'] = np.uint16(RelEnds)
-	FNP_dict['RelVec'] = np.uint16(RelVec)
-	FNP_dict['SegStartI'] = np.uint16(SegStartI)
-	FNP_dict['SegEndI'] = np.uint16(SegEndI)
+	FNP_dict['RelStarts'] = np.uint16(aux.RelStarts)
+        
+	FNP_dict['RelEnds'] = np.uint16(aux.RelEnds)
+	FNP_dict['RelVec'] = np.uint16(aux.RelVec)
+	FNP_dict['SegStartI'] = np.uint16(aux.SegStartI)
+	FNP_dict['SegEndI'] = np.uint16(aux.SegEndI)
 	FNP_dict['auxFathers'] = np.uint16(aux.Fathers)
 	FNP_dict['FIdxsXT'] = np.uint16(FIdxsX.T)
-	
 	FNP_dict['CompDepth32'] = np.uint16(np.array([CompDepth32]))
 	FNP_dict['CompFDepth32'] = np.uint16(np.array([CompFDepth32]))
 	FNP_dict['CompByLevel32T'] = np.uint16(CompByLevel32.T)
 	FNP_dict['CompByFLevel32T'] = np.uint16(CompByFLevel32.T)
 	aux.CompDepth32 = CompDepth32
 	aux.CompFDepth32 = CompFDepth32
-	FNP_dict['auxLRelStartsSize'] = np.uint16(np.array([aux.RelStarts.size]))
+	FNP_dict['auxLRelStartsSize'] = np.uint16(np.array([aux.LRelStarts.size]))
 	FNP_dict['auxLRelStarts_1'] = np.uint16(np.subtract(aux.LRelStarts, 1))
 	FNP_dict['auxLRelEnds_1'] = np.uint16(np.subtract(aux.LRelEnds, 1))
 	FNP_dict['auxFLRelStartsSize'] = np.uint16(np.array([aux.FLRelStarts.size]))
 	FNP_dict['auxFLRelStarts_1'] = np.uint16(np.subtract(aux.FLRelStarts, 1))
 	FNP_dict['auxFLRelEnds_1'] = np.uint16(np.subtract(aux.FLRelEnds, 1))
 	FNP_dict['auxKsB_1'] = np.uint16(np.subtract(aux.KsB, 1))
-	
+        FNP_data = ''
+        for k in FNP_dict:
+            s = StringIO()
+            np.savetxt(s, FNP_dict[k].flatten(), fmt='%.5f', newline=',')
+            st = s.getvalue()
+            FNP_data += st + '\n'
+        with open(FNP_csv, 'w') as fnp_f:
+            fnp_f.write(FNP_data)
+
 	sio.savemat(FNP, FNP_dict)
 	
 	FNP_dict['auxKsB_1'] = np.uint16(np.subtract(aux.KsB, 1))
+
+        FNP_dict_uint16 = {}
+	FNP_dict_uint16['N'] = np.array(np.uint16([N]))
+        FNP_dict_uint16['Ks'] = np.uint16(Ks)
+        FNP_dict_uint16['SegToComp'] = np.uint16(seg_to_comp)
+	FNP_dict_uint16['nrnHasHHSize'] = np.array(np.uint16([bool_model.shape[0]]))
+	FNP_dict_uint16['nrnHasHHT'] = bool_model.T
+	FNP_dict_uint16['SonNoVec'] = np.uint16(SonNoVec)
+	FNP_dict_uint16['Depth'] = np.array(np.uint16([Depth]))
+	FNP_dict_uint16['LognDepth'] = np.array(np.uint16([LognDepth]))
+	FNP_dict_uint16['nFathers'] = np.array(np.uint16([nFathers]))
+	FNP_dict_uint16['nCallForFather'] = np.array(np.uint16([nCallForFather]))
+	FNP_dict_uint16['RelStarts'] = np.uint16(RelStarts)
+	FNP_dict_uint16['RelEnds'] = np.uint16(RelEnds)
+	FNP_dict_uint16['RelVec'] = np.uint16(RelVec)
+	FNP_dict_uint16['SegStartI'] = np.uint16(SegStartI)
+	FNP_dict_uint16['SegEndI'] = np.uint16(SegEndI)
+	FNP_dict_uint16['auxFathers'] = np.uint16(aux.Fathers)
+	FNP_dict_uint16['FIdxsXT'] = np.uint16(FIdxsX.T)
+	FNP_dict_uint16['CompDepth32'] = np.uint16(np.array([CompDepth32]))
+	FNP_dict_uint16['CompFDepth32'] = np.uint16(np.array([CompFDepth32]))
+	FNP_dict_uint16['CompByLevel32T'] = np.uint16(CompByLevel32.T)
+	FNP_dict_uint16['CompByFLevel32T'] = np.uint16(CompByFLevel32.T)
+	FNP_dict_uint16['auxLRelStartsSize'] = np.uint16(np.array([aux.RelStarts.size]))
+	FNP_dict_uint16['auxLRelStarts_1'] = np.uint16(np.subtract(aux.LRelStarts, 1))
+	FNP_dict_uint16['auxLRelEnds_1'] = np.uint16(np.subtract(aux.LRelEnds, 1))
+	FNP_dict_uint16['auxFLRelStartsSize'] = np.uint16(np.array([aux.FLRelStarts.size]))
+	FNP_dict_uint16['auxFLRelStarts_1'] = np.uint16(np.subtract(aux.FLRelStarts, 1))
+	FNP_dict_uint16['auxFLRelEnds_1'] = np.uint16(np.subtract(aux.FLRelEnds, 1))
+	FNP_dict_uint16['auxKsB_1'] = np.uint16(np.subtract(aux.KsB, 1))
+        sio.savemat(FNP_uint16, FN_dict_uint16)
+
+        FNP_dict_double = {}
+	FNP_dict_double['e'] = np.double(e)
+	FNP_dict_double['f'] = np.double(f)
+	FNP_dict_double['cmVec'] = np.double(cmVec)
+        sio.savemat(FNP_double, FNP_dict_double)
+
 	return FN, FNP, FNM, aux
